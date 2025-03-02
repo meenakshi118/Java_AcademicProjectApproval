681f1de8aab89c8d7a79d6f130e7c8f34ef32b76
meenakshi118 <upretimeenakshi123@gmail.com>
2025-03-02 10:36:37 +0530

    Add files via upload

---
+++ code/model/HodInfo.java
@@ -0,0 +1,70 @@
+package com.project.model;
+
+public class HodInfo {
+    private int id;
+    private String name;
+    private String gmail;
+    private String password;
+    private String course;
+    private String department;  // Added department field
+    private String phoneNumber;
+
+
+    
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getGmail() {
+        return gmail;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
